cd8ba937c24164c7c26863012be6d500f5e58a54
amoljagadambe <amoljagadambe0076@gmail.com>
2019-12-05 21:52:25 +0530

    Utils Updated

--- Sudoku/utils.py
+++ Sudoku/utils.py
@@ -13,7 +13,7 @@ square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', 
 unitlist = row_units + column_units + square_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
-
+# print(column_units)
 
 # print(unitlist)
 def display(values):
