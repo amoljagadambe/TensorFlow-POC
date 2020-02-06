ebba5c0893d70a742379c3243e10c175a080253b
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-06 15:36:00 +0530

    Converted the raw logic into function to access for mean and STD

--- ML_Concepts/mean_std.py
+++ ML_Concepts/mean_std.py
@@ -5,19 +5,69 @@ speed = [86, 87, 88, 86, 87, 85, 86]
 
 x = numpy.std(speed)
 
-print("standered deaviation by numy lib: ",x)
+print("STANDARD DEVIATION by numpy library: ", x)
 
-total_value = len(speed)
-# print(total_value)
-addition = sum(speed)
-# print("additiion >>>", addition)
-mean_value = addition / total_value
-# print(mean_value)  # output: 86.42857142857143
 
-squre_sum = []
-for i in speed:
-    normal = i - mean_value
-    squre_sum.append(normal**2)
+def meanCalualation(list_data=[]):
+    """"
+       Compute the mean
 
-# print(sumsition_value)
-print("standered deaviation by numy calculation: ",math.sqrt(sum(squre_sum) / total_value))
+       Returns the mean is computed for the
+       flattened array by default
+
+       Parameters
+       ----------
+       elements: flattened list or array
+
+        Returns
+       -------
+       The mean value
+
+       Example:
+       array = [86, 87, 88, 86, 87, 85, 86]
+       std = meanCalualation(array)
+       output: 86.42857142857143
+       """
+
+    total_value = len(list_data)
+    addition = sum(list_data)
+    mean_value = addition / total_value
+    return mean_value
+
+
+def stdCalualation(elements=[]):
+    """"
+    Compute the standard deviation
+
+    Returns the standard deviation, a measure of the spread of a distribution,
+    of the array elements. The standard deviation is computed for the
+    flattened array by default
+
+    Parameters
+    ----------
+    elements: flattened list or array
+
+     Returns
+    -------
+    The STD value
+
+    Example:
+    array = [86, 87, 88, 86, 87, 85, 86]
+    std = stdCalualation(array)
+    output: 0.9035079029052513
+    """
+
+    squre_sum = []
+    n_elements = len(elements)
+    for i in elements:
+        normal = i - meanCalualation(elements)
+        squre_sum.append(normal ** 2)
+    return math.sqrt(sum(squre_sum) / n_elements)
+
+
+'''
+stdCalualation() accept array or list which is flatten by default
+'''
+
+std = stdCalualation(speed)
+print("STANDARD DEVIATION by calculation: ", std)
