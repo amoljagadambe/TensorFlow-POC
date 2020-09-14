5ac0d20c1f7a26b56781470300dcef5ecfa94fd8
amoljagadambe <amol.jagdambe@gmail.com>
2020-09-14 20:09:16 +0530

    basic regression model with dummy data

--- deeplearning/tf_basics.py
+++ deeplearning/tf_basics.py
@@ -21,7 +21,7 @@ a = tf.constant([[1, 2],
                  [3, 4]])
 
 b = tf.constant([[10],
-                [100]])
+                 [100]])
 
 mat_mul = tf.matmul(a, b)
 
@@ -37,4 +37,16 @@ print(result)
 print('\n')
 print('MATMUL IS:\n{}'.format(mat_res))
 
+"""
+TF Variable and placeholders
+"""
 
+my_tensor = tf.random_uniform((4, 4))
+my_var = tf.Variable(initial_value=my_tensor)
+init = tf.global_variables_initializer()  # this will initialize the variable declared above
+with tf.Session() as session:
+    session.run(init)
+    session.run(my_var)
+
+
+ph = tf.placeholder(tf.float32)   # placeholder's
